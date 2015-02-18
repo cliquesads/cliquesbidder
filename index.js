3f1c71ca15c10ec1950d06c493a88846a3e076b5
Ben Liang <bliang@cliquesads.com>
2015-02-18 00:48:44 -0500

    BF: Cleaned up index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var math = require('mathjs');
 var fs = require('fs');
 var http = require('http');
-var br = require('./lib/bid_requests');
 
 var app = express();
 
@@ -10,26 +9,16 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-    response.send('Hello World!');
+    response.send('bidder');
 });
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'));
 });
 
-app.get('/exchange/bids', function(request, response) {
-    response.send('<h1>This is an exchange</h1><p>Dont really know what im doing</p>');
+app.post('/bid/', function(request, response) {
+    response.send('response');
 });
-
-app.get('/exchange/test_auction', function(request, response){
-    response.send(main(request, response))
-});
-
-
-
-function main(request, response){
-    var bids;
-}
 
 
 
---
+++ setup_ubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get install python-software-properties
+sudo apt-add-repository ppa:chris-lea/node.js
+sudo apt-get update
