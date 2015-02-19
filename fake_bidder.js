a17b8796c621da2da4fb305d0451d120c50445e3
Ben Liang <bliang@cliquesads.com>
2015-02-19 15:09:42 -0500

    RF: Moved fake bidder into separate module & handle passback of data, and added advertiser metadata for test bids

---
+++ fake_bidder.js
@@ -0,0 +1,66 @@
+/**
+ * Created by bliang on 2/19/15.
+ */
+var fs = require('fs');
+var querystring = require('querystring');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json()); // for  /> application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+exports.get_single_seatbid_fake_response = get_single_seatbid_fake_response;
+
+function _get_fake_bidder_nurl(request){
+    //construct URL to pass in bid response as nurl for win notification
+    var nurl_base = "http://" + request.headers.host;
+    // assumes single impression contained in bid request
+    var impid = request.body.imp[0].id;
+    qs = querystring.encode({"impid":impid});
+    return nurl_base + '/win?' + qs;
+}
+
+function get_single_seatbid_fake_response(request, callback){
+    // generates simulated bid response to bid request
+    // request must be original HTTP POST request object representing the incoming bid request
+    var response_data;
+    fs.readFile('openrtb_bid_response_template.json','utf8',function(err, data){
+        if (err) {
+            callback(err);
+        }
+        response_data = JSON.parse(data);
+
+        // replace bid values with random numbers
+        response_data.seatbid[0].bid[0].price = +((Math.random() * 10).toFixed(2));
+        response_data.id =  Math.round(Math.random() * 10e10);
+        response_data.seatbid[0].seat =  Math.round(Math.random() * 10e3);
+        response_data.seatbid[0].bid[0].id =  Math.round(Math.random() * 10e2);
+
+        //now populate pass-back values
+
+        // WARNING: assumes SINGLE IMPRESSION contained in bid request, i.e.
+        // that body.imp.length == 1
+        response_data.seatbid[0].bid[0].impid = request.body.imp[0].id;
+        response_data.bidid = request.body.id;
+        response_data.seatbid[0].bid[0].nurl = _get_fake_bidder_nurl(request);
+
+        //now add in "real" advertiser metadata
+        fs.readFile('sample_advertiser_metadata.json','utf8',function(err,sample_metadata){
+            if (err) throw err;
+            var json_metadata = JSON.parse(sample_metadata);
+            var advertiser_index = Math.round(Math.random() * 9); // just randomly choose from array of length 10
+            var this_metadata = json_metadata[advertiser_index];
+
+            //now set advertiser-specific variables in response object
+            response_data.seatbid[0].bid[0].adm = this_metadata.adm;
+            response_data.seatbid[0].bid[0].adomain = this_metadata.adomain;
+            response_data.seatbid[0].bid[0].h = this_metadata.h;
+            response_data.seatbid[0].bid[0].w = this_metadata.w;
+            response_data.seatbid[0].bid[0].cid = this_metadata.cid;
+            response_data.seatbid[0].bid[0].crid = this_metadata.crid;
+
+            // finally, call callback
+            callback(null, response_data);
+        });
+    });
+}
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var express = require('express');
-var fs = require('fs');
-var http = require('http');
 var bodyParser = require('body-parser');
+var fake_bidder = require('./fake_bidder');
 
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.json()); // for  /> application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', function(request, response) {
@@ -18,53 +17,33 @@ app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'));
 });
 
-function single_seatbid_fake_response(callback){
-    var response_data;
-    fs.readFile('openrtb_bid_response.json','utf8',function(err, data){
-        if (err) {
-            callback(err);
-        }
-        response_data = JSON.parse(data);
-        // replace necessary values with random values
-        response_data.seatbid[0].bid[0].price = +((Math.random() * 10).toFixed(2));
-        response_data.id =  Math.round(Math.random() * 10e10);
-        response_data.bidid = Math.random().toString(36).substring(7);
-        response_data.seatbid[0].seat =  Math.round(Math.random() * 10e3);
-        response_data.seatbid[0].bid[0].impid =  Math.round(Math.random() * 10e9);
-        response_data.seatbid[0].bid[0].id =  Math.round(Math.random() * 10e2);
-        callback(null,response_data);
-    });
-}
-
 app.post('/bid/', function(request, response){
     //log request data
     var body = request.body;
     var hostname = request.hostname;
     var ip_address = request.ip;
-    var request_id;
-    if (request.query.hasOwnProperty('request_id')) {
-        request_id = request.query.request_id;
-    }
-    var bidder_id;
-    if (request.query.hasOwnProperty('bidder_id')) {
-        bidder_id = request.query.bidder_id;
-    }
+    var request_id = body.id;
+    var bidder_id = request.query.bidder_id;
 
     console.log('POST Request from ' + hostname + '(' + ip_address + ') to BidderID: ' + bidder_id + ', RequestID: ' + request_id);
     console.log(body);
 
     var json_response;
+    //get hostname of incoming request to send win-notice back to
+    //TODO: Replace with variable for secure/non-secure http protocol as applicable
+
     // now build bid response
-    single_seatbid_fake_response(function(err,response_data){
-        if (err) {
-            response.status(500).json({"ERROR": "Internal Server Error: " + err});
-            response.send();
-        }
+    fake_bidder.get_single_seatbid_fake_response(request, function(err,response_data){
+        if (err) throw err;
         json_response = response_data;
         response.status(200).json(json_response);
         response.send();
     });
 });
+
+
+
+
 
 
 
