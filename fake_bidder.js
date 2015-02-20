2ae6398458463c8ef37c065ba9ff353ab258249e
Ben Liang <bliang@cliquesads.com>
2015-02-19 23:42:23 -0500

    NF: Install PM2

--- fake_bidder.js
+++ fake_bidder.js
@@ -6,6 +6,7 @@ var querystring = require('querystring');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+
 app.use(bodyParser.json()); // for  /> application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
