b4a9a567ecff9344ba0f0daa18c079472c21c61d
Ben Liang <bliang@cliquesads.com>
2015-02-20 11:39:41 -0500

    NF: Modify setup script to install node & pm2

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#system deps
 sudo apt-get install python-software-properties
 sudo apt-add-repository ppa:chris-lea/node.js
 sudo apt-get update
+sudo install nodejs
+
+#install node dependencies
+npm install
+#have to install pm2 globally
+sudo npm install pm2 -g --unsafe-perm
+
