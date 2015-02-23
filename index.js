cf320a2c7103bb8bafaf9a8b46e71e1f0a7830c8
Ben Liang <bliang@cliquesads.com>
2015-02-23 14:30:50 -0500

    NF: update logging

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+//first party deps
 var node_utils = require('cliques_node_utils');
+var fake_bidder = require('./fake_bidder');
 
+//third-party deps
 var express = require('express');
 var bodyParser = require('body-parser');
-var fake_bidder = require('./fake_bidder');
+var requestIp = require('request-ip');
+var winston = require('winston');
+var path = require('path');
+var util = require('util');
+var responseTime = require('response-time');
+
+// Set up winston logger instance
+var logfile = path.join(process.env['HOME'],'logs',util.format('bidder_%s.log',Date.now()));
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)({timestamp:true}),
+        new (winston.transports.File)({filename:logfile,timestamp:true})
+    ]
+});
 
 var app = express();
 
@@ -11,49 +27,47 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()); // for  /> application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+// inside request-ip middleware handler
+app.use(function(req, res, next) {
+    req.clientIp = requestIp.getClientIp(req); // on localhost > 127.0.0.1
+    next();
+});
+app.use(responseTime());
+
 app.get('/', function(request, response) {
     response.send('bidder');
 });
 
 app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'));
+    logger.info("Node app is running at localhost:" + app.get('port'));
 });
 
 app.post('/bid/', function(request, response){
     //log request data
     var body = request.body;
-    var hostname = request.hostname;
-    var ip_address = request.ip;
-    var request_id = body.id;
-    var bidder_id = request.query.bidder_id;
-
-    console.log('POST Request from ' + hostname + '(' + ip_address + ') to BidderID: ' + bidder_id + ', AuctionID: ' + request_id);
+    var meta = {bid_request: body};
+    node_utils.logging.log_request(logger, request, meta);
     //console.log(body);
-
     var json_response;
-    //get hostname of incoming request to send win-notice back to
     //TODO: Replace with variable for secure/non-secure http protocol as applicable
 
     // now build bid response
     fake_bidder.get_single_seatbid_fake_response(request, function(err,response_data){
-        if (err) throw err;
+        if (err){
+            logger.error(err);
+        }
         json_response = response_data;
+        response.set(fake_bidder.DEFAULT_HEADERS);
         response.status(200).json(json_response);
         response.send();
+        node_utils.logging.log_response(logger, response)
     });
 });
 
 app.get('/win/', function(request, response){
-    console.log("GET request: Win notification " + request.originalUrl);
+    node_utils.logging.log_request(logger, request);
     response.set(fake_bidder.DEFAULT_HEADERS);
     response.status(200);
     response.send();
+    node_utils.logging.log_response(logger, response);
 });
-
-
-
-
-
-
-
-
