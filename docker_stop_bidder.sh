aa7e91f2c895757038a408546158c9b84b202d98
Ben Liang <bliang@cliquesads.com>
2015-03-19 21:29:31 -0400

    NF: Needed script to stop bidder services as well so you can gracefully exit, otherwise you get a bunch of stale PID files lying all over the place next time you open the container

---
+++ docker_stop_bidder.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+REPOSITORY_DIR=$HOME/repositories
+CONFIG_DIR=$REPOSITORY_DIR/cliques-config/rtbkit
+APACHE_DIR=/etc/apache2
+LOCAL_DIR=$REPOSITORY_DIR/cliquesbidder/rtbkit
+
+cd $LOCAL_DIR
+source rtbkit-*.sh
+cd ..
+
+/etc/init.d/postgresql stop
+
+# Stop zookeeper
+#$LOCAL_DIR/bin/zookeeper/bin/zkServer.sh stop
+$HOME/local/bin/zookeeper/bin/zkServer.sh stop
+# Start Redis
+redis-cli shutdown
+# Start Carbon
+/opt/graphite/bin/carbon-cache.py stop
+# Start apache
+service apache2 stop
+
+#figoure out how to kill bidder service with TMUX
