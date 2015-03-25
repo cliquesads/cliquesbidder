309a26557bcc70ba3edfc80624a5a50d710cc2bd
Ben Liang <bliang@cliquesads.com>
2015-03-25 00:33:36 -0400

    Update exchange name

--- bidding-agents/nodebidagent-config.js
+++ bidding-agents/nodebidagent-config.js
@@ -9,7 +9,7 @@ exports.config = {
   "account": ["hello","world"],
   "bidProbability": 1.0,
     "providerConfig":{
-        "casale":{
+        "cliques":{
             "seat": 129349
         }
     },
@@ -32,7 +32,7 @@ exports.config = {
       "name":"BigBox",
       "tagId":2,
       "providerConfig":{
-          "casale":{
+          "cliques":{
               "adm": "<iframe id='a1faac72' name='a1faac72' src='http://ads.cliquesads.com/www/delivery/afr.php?zoneid=3' frameborder='0' scrolling='no' width='300' height='250'><a href='http://ads.cliquesads.com/www/delivery/ck.php?n=a3312a64' target='_blank'><img src='http://ads.cliquesads.com/www/delivery/avw.php?zoneid=3&amp;n=a3312a64' border='0' alt='' /></a></iframe>",
               "adomain": ["www.untapped.cc"]
           }
