1c282c1d29a1071a52e6326654433d3657893160
Ben Liang <bliang@cliquesads.com>
2015-05-08 15:33:34 -0400

    NF: Initial attempt to integrate nodebidagent.  W/ logging strings to figure out WTF all of the args mean.

--- bidding-agents/nodebidagent.js
+++ bidding-agents/nodebidagent.js
@@ -1,36 +1,62 @@
 var RTBkit = require('./../rtbkit/bin/rtb'),
     services_lib = require('./../rtbkit/bin/services'),
     budgetController = require('./budget-controller'),
-    config = require('./nodebidagent-config').config,
-    zookeeperUri = "localhost:2181", // must point to same Zookeeper as routers
+    agentConfig = require('./nodebidagent-config').config,
+    config = require('config'),
+    winston = require('winston'),
+    node_utils = require('cliques_node_utils');
+    //googleAuth = node_utils.google.auth;
+
+/* -------------------  LOGGING ------------------- */
+
+var logfile = path.join(
+    process.env['HOME'],
+    'rtbkit_logs',
+    'nodebidagent',
+    util.format('nodebidagent_%s.log',node_utils.dates.isoFormatUTCNow())
+);
+
+//var bq_config = bigQueryUtils.loadFullBigQueryConfig('../bq_config.json');
+//var eventStreamer = new bigQueryUtils.BigQueryEventStreamer(bq_config,
+//    googleAuth.DEFAULT_JWT_SECRETS_FILE,20);
+logger = new logging.BidderCLogger({
+    transports: [
+        new (winston.transports.Console)({timestamp:true}),
+        new (winston.transports.File)({filename:logfile,timestamp:true})
+        //new (winston.transports.RedisEventCache)({ eventStreamer: eventStreamer})
+    ]
+});
+
+
+/* ------------------ RTBKit Vars --------------------*/
+
+var zookeeperUri = "localhost:2181", // must point to same Zookeeper as routers
     services = new services_lib.ServiceProxies(),
     accountAdded = false,
     interval,
-    accountParent = config.account[0],
-    accountFullName = config.account.join(":");
+    accountParent = agentConfig.account[0],
+    accountFullName = agentConfig.account.join(":");
     
 // uri,install name and location from bootstrap.json
 services.useZookeeper(zookeeperUri,"rtb-test", "mtl"); 
 
-// yes, we want to log to carbonls
+// yes, we want to log to carbon
 services.logToCarbon('127.0.0.1:2003');
 
 var addAccountHandler = function(err, res){
   if (err) {
-    console.log("Error adding account "+accountFullName);
-    console.log(err);
+    logger.error("Error adding account "+accountFullName);
+    logger.error(err);
   }
 };
-
 var topupErrorHandler = function(err, res){
   if (err) {
     // TODO: Handle an error topping up the account. 
-    console.log("Error topping up "+accountFullName);
+    logger.error("Error topping up "+accountFullName);
     // shutdown with an error
     process.exit(1);
   }
 };
-
 // Keep the budget for this subaccount topped up
 var pace = function(){
   if (!accountAdded){
@@ -39,88 +65,180 @@ var pace = function(){
   }
   // Transfer 10 cents every time we pace
   budgetController.topupTransferSync(accountFullName, "USD/1M", 100000, topupErrorHandler);
-}
+};
 
-var agent = new RTBkit.BiddingAgent("predicativeAgent", services);
+var agent = new RTBkit.BiddingAgent("cliquesBidAgent", services);
 
 //---------------------
 // Agent Event Handlers 
 //---------------------
+
 // You can skip overriding some of these handlers by setting strictMode(false);
 
+/**
+ * Handles the actual bidding part.
+ * @param timestamp
+ * @param auctionId
+ * @param bidRequest
+ * @param bids
+ * @param timeAvailableMs
+ * @param augmentations
+ * @param wcm
+ */
+agent.onBidRequest = function(timestamp, auctionId, bidRequest, bids, timeAvailableMs, augmentations, wcm){
+    var amount = new RTBkit.MicroUSD(100);
+    logger.info("Bid request: " + JSON.stringify(bidRequest));
+
+    logger.info("Bids Obj:" + JSON.stringify(bids));
+    for (var i=0; i<bids.length; i++){
+        // TODO: validate a bid before deciding to put an amount on it
+        bids.bid(i,bids[i].availableCreatives[0],amount,1); // spotId, creativeIndex, price, priority
+    }
+    agent.doBid(auctionId, bids, {}, wcm); // auction id, collection of bids, meta, win cost model.
+    amount = null;
+};
+
 agent.onError = function(timestamp, description, message){
-  console.log('Bidding Agent sent something invalid to the router.', description, message);
-}
+  logger.error('Bidding Agent sent something invalid to the router.', description, message);
+};
 
 // the agent won a bid. secondPrice contains the win price
 agent.onWin = function(timestamp, confidence, auctionId, spotNum, secondPrice, bidRequest, ourBid, accountInfo, metadata, augmentations, uids){
-  console.log("WON");
-}
+  logger.info("WIN", {
+      type: "WIN",
+      timestamp: timestamp,
+      confidence: confidence,
+      auctionId: auctionId,
+      spotNum: spotNum,
+      secondPrice: secondPrice,
+      bidRequest: bidRequest,
+      ourBid: ourBid,
+      accountInfo: accountInfo,
+      metadata: metadata,
+      augmentations: augmentations,
+      uids: uids
+  });
+};
 
 // the auction was not won by this agent
 agent.onLoss = function(timestamp, confidence, auctionId, spotNum, secondPrice, bidRequest, ourBid, accountInfo, metadata){
-  console.log("LOSS");
-}
+    logger.info("LOSS", {
+        type: "LOSS",
+        timestamp: timestamp,
+        confidence: confidence,
+        auctionId: auctionId,
+        spotNum: spotNum,
+        secondPrice: secondPrice,
+        bidRequest: bidRequest,
+        ourBid: ourBid,
+        accountInfo: accountInfo,
+        metadata: metadata
+    });
+};
 
 // an invalid bid has been sent back to the router
 agent.onInvalidBid = function(timestamp, confidence, auctionId, spotNum, secondPrice, bidRequest, ourBid, accountInfo, metadata, augmentations, uids){
-  console.log("INVALIDBID");
-}
+    logger.info("INVALID-BID", {
+        type: "INVALID-BID",
+        timestamp: timestamp,
+        confidence: confidence,
+        auctionId: auctionId,
+        spotNum: spotNum,
+        secondPrice: secondPrice,
+        bidRequest: bidRequest,
+        ourBid: ourBid,
+        accountInfo: accountInfo,
+        metadata: metadata,
+        augmentations: augmentations,
+        uids: uids
+    });
+};
 
 // a bid was placed by this bid agent after the router had sent its bids back to the exchange
 agent.onTooLate = function(timestamp, confidence, auctionId, spotNum, secondPrice, bidRequest, ourBid, accountInfo, metadata, augmentations, uids){
-  console.log("TOOLATE");
-}
+    logger.info("TOO-LATE", {
+        type: "TOO-LATE",
+        timestamp: timestamp,
+        confidence: confidence,
+        auctionId: auctionId,
+        spotNum: spotNum,
+        secondPrice: secondPrice,
+        bidRequest: bidRequest,
+        ourBid: ourBid,
+        accountInfo: accountInfo,
+        metadata: metadata,
+        augmentations: augmentations,
+        uids: uids
+    });
+};
 
 // not sufficient budget available for this agent to bid the price it has chosen
 agent.onNoBudget = function(timestamp, confidence, auctionId, spotNum, secondPrice, bidRequest, ourBid, accountInfo, metadata, augmentations, uids){
-  console.log("NOBUDGET");
-}
+    logger.info("NO-BUDGET", {
+        type: "NO-BUDGET",
+        timestamp: timestamp,
+        confidence: confidence,
+        auctionId: auctionId,
+        spotNum: spotNum,
+        secondPrice: secondPrice,
+        bidRequest: bidRequest,
+        ourBid: ourBid,
+        accountInfo: accountInfo,
+        metadata: metadata,
+        augmentations: augmentations,
+        uids: uids
+    });
+};
 
 // the auction dropped this bid. usually happens if the auctionId is unknown
 // or if the bid was delayed for too long.
 agent.onDroppedBid = function(timestamp, confidence, auctionId, spotNum, secondPrice, bidRequest, ourBid, accountInfo, metadata, augmentations, uids){
-  console.log("DROPPED");
-}
+    logger.info("DROPPED-BID", {
+        type: "DROPPED-BID",
+        timestamp: timestamp,
+        confidence: confidence,
+        auctionId: auctionId,
+        spotNum: spotNum,
+        secondPrice: secondPrice,
+        bidRequest: bidRequest,
+        ourBid: ourBid,
+        accountInfo: accountInfo,
+        metadata: metadata,
+        augmentations: augmentations,
+        uids: uids
+    });
+};
 
 // respond to the router when pinged.
 agent.onPing = function(router,timesent,args){
   var timereceived = new Date();
   agent.doPong(router, timesent, timereceived, args);
   timereceived = null;
-}
-
-agent.onImpression = function(timestamp, auctionId, spotId, spotIndex, bidRequest, bidMeta, winMeta, impressionMeta, clickMeta, augmentations, visits){
-  console.log("IMPRESSION");
-}
-
-agent.onVisit = function(timestamp, auctionId, spotId, spotIndex, bidRequest, bidMeta, winMeta, impressionMeta, clickMeta, augmentations, visits){
-  console.log("VISIT");
-}
-
-agent.onClick = function(timestamp, auctionId, spotId, spotIndex, bidRequest, bidMeta, winMeta, impressionMeta, clickMeta, augmentations, visits){
-  console.log("CLICK");
-}
-
-
-agent.onBidRequest = function(timestamp, auctionId, bidRequest, bids, timeAvailableMs, augmentations, wcm){
-  console.log("bidrequest", augmentations, bidRequest);
-  var amount = new RTBkit.MicroUSD(100);
-  for (var i=0; i<bids.length; i++){
-    // TODO: validate a bid before deciding to put an amount on it
-    bids.bid(i,bids[i].availableCreatives[0],amount,1); // spotId, creativeIndex, price, priority
-  }
-  agent.doBid(auctionId, bids, {}, wcm); // auction id, collection of bids, meta, win cost model.
-  amount = null;
 };
 
+//agent.onImpression = function(timestamp, auctionId, spotId, spotIndex, bidRequest, bidMeta, winMeta, impressionMeta, clickMeta, augmentations, visits){
+//  console.log("IMPRESSION");
+//}
+//
+//agent.onVisit = function(timestamp, auctionId, spotId, spotIndex, bidRequest, bidMeta, winMeta, impressionMeta, clickMeta, augmentations, visits){
+//  console.log("VISIT");
+//}
+//
+//agent.onClick = function(timestamp, auctionId, spotId, spotIndex, bidRequest, bidMeta, winMeta, impressionMeta, clickMeta, augmentations, visits){
+//  console.log("CLICK");
+//}
+
+//-------------------------
 // END Agent Event Handlers
+//-------------------------
 
-
+//-------------------------
+// Initialize agent
+//-------------------------
 agent.init();
 agent.start();
 
-agent.doConfig(config);
+agent.doConfig(agentConfig);
 // Start pacing the budget inflow for this bid agent
 pace();
 interval = setInterval(pace,10000);
