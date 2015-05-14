9531489431ccaa74018577b200c96b50fabbb678
Ben Liang <bliang@cliquesads.com>
2015-05-14 01:42:59 -0400

    Moved all fake bidder scripts to separate dir

--- fakebidder/deploy_fake_bidder.sh
+++ fakebidder/deploy_fake_bidder.sh
@@ -19,7 +19,7 @@ if [ -z "$running" ]; then
     # hook PM2 up to web monitoring with KeyMetrics
     pm2 interact 9661z7ru6dmulvs 6xw07v3gpf6e7hm
     # start in cluster mode
-    pm2 start index.js --name "$processname" -i 0
+    pm2 start fake_bidder_index.js --name "$processname" -i 0
 else
     pm2 gracefulReload "$processname"
 fi
