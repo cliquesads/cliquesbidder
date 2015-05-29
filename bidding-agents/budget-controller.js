2d8430d06b5a50002bb16e61c30a5a8761f1bd43
Ben Liang <bliang@cliquesads.com>
2015-05-29 15:13:19 -0700

    Add pmx to handle uncaught exceptions and disable pacer logging

--- bidding-agents/budget-controller.js
+++ bidding-agents/budget-controller.js
@@ -86,7 +86,7 @@ CampaignBudgetController.prototype.configure_and_run = function(agentConfig){
     // chunks at a time.
     this._pace(function(err, child_account) {
         if (err) return console.log(err);
-        console.log('Pacing done, child account: ' + JSON.stringify(child_account));
+        //console.log('Pacing done, child account: ' + JSON.stringify(child_account));
     });
 };
 
