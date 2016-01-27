2f5b16da2d9e28f44b0061fcb0f22aec58cdb557
Ben Liang <bliang@cliquesads.com>
2016-01-27 00:58:43 -0500

    BF: was resetting processname

--- deploy-controller.sh
+++ deploy-controller.sh
@@ -73,7 +73,6 @@ else
     cd ../cliquesbidder
 fi
 
-processname='bidAgentController'
 running=$(pm2 list -m | grep "$processname")
 if [ -z "$running" ]; then
     # hook PM2 up to web monitoring with KeyMetrics
