0c697cb52ca460cd4a924acc3beb43c63dc2d617
Chuoxian Yang <yangchuoxian.appdev@gmail.com>
2018-05-31 23:31:01 +0800

    All set

--- querycorebidder/querycore.py
+++ querycorebidder/querycore.py
@@ -4,7 +4,6 @@ import traceback
 import os
 import sys
 import requests
-
 import time
 import datetime
 
@@ -30,7 +29,7 @@ config = JsonConfigParser()
 pd_api_key = config.get('PagerDuty', 'api_key')
 pd_subdomain = config.get('PagerDuty', 'subdomain')
 pd_service_key = config.get('PagerDuty', 'service_key')
-if os.environ.get('ENV', None) == 'production':
+if os.environ.get('NODE_ENV', None) == 'production':
     pd_error_callback = create_pd_event_wrapper(pd_subdomain, pd_api_key, pd_service_key)
 else:
     pd_error_callback = None
@@ -40,18 +39,10 @@ if __name__ == '__main__':
     # {"error":"UNKNOWN_RESOURCE: There is no handler for the requested resource '/'"}
     # otherwise if the core bidder is NOT running, it should get a connection error
     try:
-        # r = requests.get('http://35.196.104.183:12339')
         r = requests.get('http://127.0.0.1:12339')
-
         st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-        logfile = open("/home/bliang/repositories/test.txt", "a")
-        logfile.write(st + " --- " + r.text)
-        logfile.close
-
-        print r.text
+        print(st + " --- " + r.text + ', NODE_ENV: ' + os.environ.get('NODE_ENV', None) + '\n')
     except requests.ConnectionError, e:
-        print "It has a connection err!!!..."
         # Trigger incident in PagerDuty, then write out to log file
-        if os.environ.get('ENV', None) == 'production':
-            print "Yes, the ENV is production!!!"
-            # stacktrace_to_pd_event(pd_subdomain, pd_api_key, pd_service_key)
+        if os.environ.get('NODE_ENV', None) == 'production':
+            stacktrace_to_pd_event(pd_subdomain, pd_api_key, pd_service_key)
--- querycorebidder/setup_crontab.sh
+++ querycorebidder/setup_crontab.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This sets the crontab to run querycore.py every 5 minutes
-# newline="*/5 * * * * /usr/bin/python /home/bliang/repositories/cliquesbidder/querycorebidder/querycore.py > /home/bliang/repositories/cron.log"
+# newline="*/5 * * * * cd /home/bliang/repositories/cliquesbidder; . ./activate_env.sh -e production; /usr/bin/python /home/bliang/repositories/cliquesbidder/querycorebidder/querycore.py > /home/bliang/repositories/cron.log\n"
+
 
 # This sets the crontab to run querycore.py every minutes
-newline="* * * * * /usr/bin/python /home/bliang/repositories/cliquesbidder/querycorebidder/querycore.py > /home/bliang/repositories/cron.log\n"
+newline="* * * * * cd /home/bliang/repositories/cliquesbidder; . ./activate_env.sh -e production; /usr/bin/python /home/bliang/repositories/cliquesbidder/querycorebidder/querycore.py > /home/bliang/repositories/cron.log\n"
 (crontab -l; echo "$newline") | crontab -
