28838e2e37b291ed643fc8962bee1f4cf84863c2
Ben Liang <bliang@cliquesads.com>
2018-06-01 17:04:23 -0400

    BF: Install proper updated system dependencies so system virtualenv can be used, which will install correct updated version of pip to env

--- querycorebidder/setup_querycore.sh
+++ querycorebidder/setup_querycore.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 # This sets the crontab to run querycore.py every 5 minutes
-# newline="*/5 * * * * cd /home/bliang/repositories/cliquesbidder; . ./activate_env.sh -e production; /usr/bin/python /home/bliang/repositories/cliquesbidder/querycorebidder/querycore.py > /home/bliang/repositories/cron.log\n"
+
+# first install system dependencies
+sudo apt-get update
+# system pip, only used to install virtualenv. From then on, venv pip will be used.
+sudo apt-get install python-pip
+sudo apt-get install openssl
+sudo apt-get install python-dev
+sudo apt-get install libffi-dev
+
+# now install virtualenv
+sudo -H pip install virtualenv
 
 VIRTUALENV_FOLDER="./venv"
 if [ ! -d "VIRTUALENV_FOLDER" ]; then
-	/home/bliang/.local/bin/virtualenv venv
+	virtualenv venv
 	. venv/bin/activate
 	pip install -r requirements.txt
 fi
